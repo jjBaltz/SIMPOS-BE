68e9fd1e79ccb9edc49ab957e57c542d2df80831
Jasper Baltz <nickolas.baltz@gmail.com>
2023-10-09 19:50:40 -0500

    added type to item model and data

--- Models/Item.cs
+++ Models/Item.cs
@@ -5,6 +5,7 @@ namespace SIMPOS.Models;
 public class Item
 {
 	public int ItemId { get; set; }
+	public string Type { get; set; }
 	public string Name { get; set; }
 	public decimal Price { get; set; }
 }
