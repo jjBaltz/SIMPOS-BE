6b66c8b8d2d05a91ae35e5dd5d416515c5d881e7
Jasper Baltz <nickolas.baltz@gmail.com>
2023-10-16 20:00:05 -0500

    nullable Order parameters

--- Models/Order.cs
+++ Models/Order.cs
@@ -5,13 +5,13 @@ namespace SIMPOS.Models;
 public class Order
 {
 	public int OrderId { get; set; }
-	public int CustomerId { get; set; }
-	public int UserId { get; set; }
+	public int? CustomerId { get; set; }
+	public int? UserId { get; set; }
 	public bool Status { get; set; }
-	public string Type { get; set; }
-	public string PaymentType { get; set; }
-	public decimal Total { get; set; }
-	public int Rating { get; set; }
+	public string? Type { get; set; }
+	public string? PaymentType { get; set; }
+	public decimal? Total { get; set; }
+	public int? Rating { get; set; }
 	public Customer Customer { get; set; }
 	public List<Item> Items { get; set; }
 }
